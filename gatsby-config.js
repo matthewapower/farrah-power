4b655197a050c9161f0a20548ccddfe263cd2d0c
matthewapower <matthewallenpower@gmail.comm>
2020-01-11 10:17:06 -0500

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,21 @@ module.exports = {
           id: 'tma7ecu'
         }
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-97555760-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
   ],
 }
