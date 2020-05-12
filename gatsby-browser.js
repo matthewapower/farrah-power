fb07a594795e3bafcc97c4600c394f5254b02450
matthewapower <matthewallenpower@gmail.comm>
2020-05-12 09:03:06 -0400

    Add shop functionality in with shopify theme manager

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,3 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "./src/utils/globals.css"
-
-import React from "react"
-import { StoreContextProvider } from "./src/context/StoreContext"
-export const wrapRootElement = ({ element }) => (
-  <StoreContextProvider>{element}</StoreContextProvider>
-)
