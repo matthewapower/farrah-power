add8769557394d15eb94403e059cbd272e55f975
matthewapower <matthewallenpower@gmail.comm>
2020-08-04 19:15:22 -0400

    Add a back to top button on the blog.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -48,13 +48,13 @@ const PostContent = styled.div`
     margin-left: 20px;
     margin-right: 20px;
   }
-`;
+`
 
 const NavControls = styled.ul`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  listStyle: none;
+  liststyle: none;
   padding: 0;
   margin: 40px 0;
   list-style: none;
@@ -62,55 +62,96 @@ const NavControls = styled.ul`
   @media (max-width: 768px) {
     justify-content: center;
   }
-`;
+`
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <Title>
-          {post.frontmatter.title}
-        </Title>
-
-        <Container>
-          <PostContent dangerouslySetInnerHTML={{ __html: post.html }} />
-          <div dangerouslySetInnerHTML={{ __html: post.frontmatter.embed }} />
-          <NavControls>
-            <li>
-              {previous && (
-                <ContinueLink to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </ContinueLink>
-              )}
-            </li>
-            <li>
-              {next && (
-                <ContinueLink to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </ContinueLink>
-              )}
-            </li>
-          </NavControls>
-        </Container>
-        
-        <Helmet>
-          <script type="text/javascript" src={post.frontmatter.embedScript}></script>
-        </Helmet>
-        <CtaFooter />
-      </Layout>
-    )
+export default function BlogPostTemplate(props) {
+  const post = props.data.markdownRemark
+  const siteTitle = props.data.site.siteMetadata.title
+  const { previous, next } = props.pageContext
+  const [scrolled, setScrolled] = useState(false)
+  const handleScroll = () => {
+    const buffer = 200
+    if (window.scrollY > buffer && scrolled === false) {
+      setScrolled(true)
+    } else if (window.scrollY < buffer && scrolled === true) {
+      setScrolled(false)
+    }
   }
-}
 
-export default BlogPostTemplate
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [scrolled, handleScroll])
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <Title>{post.frontmatter.title}</Title>
+
+      <Container>
+        <PostContent dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div dangerouslySetInnerHTML={{ __html: post.frontmatter.embed }} />
+        <NavControls>
+          <li>
+            {previous && (
+              <ContinueLink to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </ContinueLink>
+            )}
+          </li>
+          <li>
+            {next && (
+              <ContinueLink to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </ContinueLink>
+            )}
+          </li>
+        </NavControls>
+      </Container>
+      <button
+        className={` flex-col items-center fixed hidden md:flex font-heading uppercase text-xs bottom-0 right-0 text-black mb-12 mr-24 focus:outline-none transition-transform duration-300 transform translate-x-0 hover:opacity-75 ${
+          scrolled ? "transform translate-x-0" : "transform translate-x-64"
+        }`}
+        onClick={() => {
+          console.log(window.scrollTo(0, 0))
+        }}
+      >
+        <svg viewBox="0 0 50 50" style={{ maxWidth: "20px" }} className="mb-2">
+          <polyline
+            points="25,10 25,50"
+            fill="none"
+            stroke="black"
+            strokeWidth="2"
+            strokeLineCap="round"
+            strokeLinejoint="round"
+          />
+          <polyline
+            points="10,15 25,0 40,15"
+            fill="none"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoint="round"
+          />
+        </svg>
+        Back to Top
+      </button>
+
+      <Helmet>
+        <script
+          type="text/javascript"
+          src={post.frontmatter.embedScript}
+        ></script>
+      </Helmet>
+      <CtaFooter />
+    </Layout>
+  )
+}
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
