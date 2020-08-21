69bd3af1daa21ba97231460413de61b9a632a015
matthewapower <matthewallenpower@gmail.comm>
2020-08-21 17:59:58 -0400

    Fix FAQ styling on safari.

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -6,9 +6,9 @@ export default function Dropdown(props) {
     <div className="w-full mb-6 border-b border-black">
       <button
         onClick={() => setActive(!active)}
-        className="w-full focus:outline-none grid grid-cols-6 justify-between items-center"
+        className="w-full focus:outline-none flex justify-between items-center"
       >
-        <h2 className="font-display-sans text-xl mb-4 text-left col-span-5 leading-7">
+        <h2 className="font-display-sans text-xl mb-4 mr-4 text-left col-span-5 leading-7">
           {props.question}
         </h2>
         <span className="font-display-sans text-xl mb-4 text-right">
